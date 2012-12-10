bd65337e8773119f4ee00fc504af8f6b2dbfe436
Alan Orth <alan.orth@gmail.com>
2012-12-10 16:03:38 -0500

    Enable Highlight.js functionality
    Highlight.js functionality depends on helper script `highlight_all_the_things.js`
    to load when the page is done loading. Once loaded, itsearches for
    any <pre><code> blocks and attempts to syntax highlight them.

--- functions.php
+++ functions.php
@@ -17,4 +17,16 @@ function load_fonts() {
 
 add_action('wp_enqueue_scripts', 'load_fonts');
 
+function highlightjs_styles() {
+	wp_enqueue_style( 'core', get_stylesheet_directory_uri() . '/highlight.default.min.css', false );
+}
+
+function highlightjs_scripts() {
+	wp_enqueue_script( 'highlight-js', get_stylesheet_directory_uri() . '/highlight.min.js', array('jquery'), false, true);
+	wp_enqueue_script( 'load-highlight-js', get_stylesheet_directory_uri() . '/highlight_all_the_things.js', false, true);
+}
+
+add_action( 'wp_enqueue_scripts', 'highlightjs_styles' );
+add_action( 'wp_enqueue_scripts', 'highlightjs_scripts' );
+
 ?>
---
+++ highlight_all_the_things.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function($) {
+  $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
+});
